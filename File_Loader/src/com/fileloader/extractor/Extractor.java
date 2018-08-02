76dc5d7bff0c10cb65eed52f85ae443b8880b12b
seelan cyrilraj <cseelanasn23@icloud.com>
2018-08-02 21:15:40 +0530

    Basic extraction code added

--- File_Loader/src/com/fileloader/extractor/Extractor.java
+++ File_Loader/src/com/fileloader/extractor/Extractor.java
@@ -5,23 +5,40 @@ package com.fileloader.extractor;
 * Also creates one csv file with the table row counts
  */
 import com.fileloader.extractor.utils.DBConnection;
+import com.fileloader.extractor.utils.JobProperties;
 
 import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import org.w3c.dom.Document;
 
 public class Extractor {
     private static Connection con = null;
     private static Map<String, Integer> tables = new HashMap<String, Integer>();
+    private static Statement stmt = null;
     private static ResultSet rs = null;
+    private static ResultSet rs_columns = null;
+    private String tableschema;
 
     public Extractor(String schema){
         try {
+            this.tableschema = schema;
             DBConnection connection = new DBConnection();
             con = connection.getConnection();
-            Statement stmt=con.createStatement();
-            rs=stmt.executeQuery("select table_name,table_rows from information_schema.tables where table_schema='"+schema+"'");
+            this.stmt=con.createStatement();
+            this.rs=stmt.executeQuery("select table_name,table_rows from information_schema.tables where table_schema='"+tableschema+"'");
             while(rs.next()){
                 tables.put(rs.getString(1), rs.getInt(2));
             }
@@ -31,8 +48,43 @@ public class Extractor {
         }
     }
 
-    public void runExtractor(){
+    public void runExtractor(JobProperties jp) throws Exception{
+        for(String tablename : tables.keySet() ){
+            String tablename_formatted = formatName(tablename);
+            rs_columns = stmt.executeQuery("select column_name from information_schema.columns where table_name='"+tablename+"'");
+            System.out.println("Extraction started for table: "+tablename_formatted);
+            String filepath = jp.getValue("app.outputfiles.location", "");
+            String filename = filepath + tableschema.toUpperCase() + "-" + tablename.toUpperCase() + "-0000" + ".xml";
+            Writer out = new OutputStreamWriter(new FileOutputStream(filename));
+            XMLOutputFactory factory = XMLOutputFactory.newInstance();
+            XMLStreamWriter writer = factory.createXMLStreamWriter(out);
 
+            writer.writeStartDocument("UTF-8","1.0");
+            writer.writeCharacters("\n");
+            writer.writeStartElement(tableschema.toUpperCase());
+            writer.writeCharacters("\n\t");
+            writer.writeStartElement(tablename.toUpperCase());
+            writer.writeCharacters("\n\t\t");
+            writer.writeStartElement("ROW");
+            while(rs_columns.next()){
+                writer.writeCharacters("\n\t\t\t");
+                writer.writeStartElement(rs_columns.getString(1).toUpperCase());
+                writer.writeEndElement();
+            }
+            writer.writeCharacters("\n\t\t");
+            writer.writeEndElement();
+            writer.writeCharacters("\n\t");
+            writer.writeEndElement();
+            writer.writeCharacters("\n");
+            writer.writeEndElement();
+            writer.writeEndDocument();
+            writer.flush();
+            writer.close();
+            if(out != null){
+                out.flush();
+                out.close();
+            }
+        }
     }
 
     public void countTableRows(){
@@ -60,6 +112,7 @@ public class Extractor {
                 fw.append(table.getValue().toString());
                 fw.append(NEW_LINE_SEPARATOR);
             }
+            System.out.println("CSV file generated and stored in D:\\ETL\\row_count.csv");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -67,6 +120,16 @@ public class Extractor {
             fw.flush();
             fw.close();
         }
+    }
+
+    public static String formatName(String string) {
+        string = string.trim().replace("$", "").replace("(", "").replace(")", "").replace("[", "")
+                .replace("]", "").replace("{", "").replace("}", "").replace("/", "").replace("\\", "").replace("\"", "")
+                .replace("'", "").replace("`", "").replace("&", "").replace("@", "").replace("#", "")
+                .replace("%", "").replace("!", "").replace("^", "").replace("*", "").replace("|", "")
+                .replaceAll("\\s+", "_").toUpperCase();
+        return
+                (string.charAt(0) >= '0' && string.charAt(0) <= '9')?"_"+ string:string;
     }
 
 }
--- File_Loader/src/com/fileloader/extractor/XMLExtract.java
+++ File_Loader/src/com/fileloader/extractor/XMLExtract.java
@@ -7,11 +7,16 @@ import com.fileloader.extractor.Extractor;
 public class XMLExtract {
 
     public static void main(String args[]){
-        JobProperties jp = new JobProperties("D://File_Loader//src//job.properties");
-        String schema = jp.getValue("db.schema.name", "DBO");
-        Extractor newjob = new Extractor(schema);
-        newjob.countTableRows();
-//        newjob.runExtractor();
+        try{
+            JobProperties jp = new JobProperties("D://File_Loader//src//job.properties");
+            String schema = jp.getValue("db.schema.name", "DBO");
+            Extractor newjob = new Extractor(schema);
+//        newjob.countTableRows();
+            newjob.runExtractor(jp);
+        }
+        catch(Exception e){
+            System.out.println("failed - exception: " + e.getMessage());
+        }
     }
 
 }
